1fe5a216f170fb3351ba84aacc4b76c134a0891b
Ihor <taratushka.igor@gmail.com>
2024-12-01 16:19:46 +0200

    Add files via upload

---
+++ Yalinka.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main() {
+    int rows = 6;
+
+    for (int i = 1; i <= rows; ++i) {
+        std::cout << std::string(rows - i, ' ');
+        std::cout << std::string(2 * i - 1, '*') << '\n';
+    }
+
+    return 0;
+}
